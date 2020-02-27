855a7bb1911b2773d0dd50719d8344bb40aa02be
BysNacK <andresnicolini94@gmail.com>
2020-02-27 14:55:14 +0000

    add documentation

--- src/word_iterator.hpp
+++ src/word_iterator.hpp
@@ -2,19 +2,37 @@
 #include <iostream>
 #include <algorithm>
 
+/**
+ *  Class used to iterate a sencence word by word
+ */
 class word_iterator {
 public:
     using difference_type = std::string_view::difference_type;
     using value_type = std::string_view;
     using iterator_category = std::bidirectional_iterator_tag;
 
+    /**
+     *  Default constructor
+     */
     word_iterator() = default;
+    
+
+    /**
+     *  String constructor
+     *
+     *  @param  data    The string containign the sentence to iterate through
+     */
     word_iterator(std::string_view data) noexcept :
         _data{ data },
         _it{ std::begin(_data) }
     {
     }
 
+    /**
+     *  Pre-decrement operator, moves the iterator to the previous word and returns it
+     * 
+     *  @return         A reference to this iterator for chaining
+     */
     word_iterator& operator--() noexcept {
         auto reverse = std::make_reverse_iterator(_it - 1);
         _it = std::find_if(reverse, std::rend(_data), isspace).base();
@@ -23,6 +41,11 @@ public:
     }
 
 
+    /**
+     *  Post-decrement operator, moves the iterator to point to the previous word
+     * 
+     *  @return         A new iterator pointing to the current word
+     */
     word_iterator operator--(int) noexcept {
         auto copy = *this;
         this->operator--();
@@ -30,12 +53,22 @@ public:
         return copy;
     }
 
+    /**
+     *  Pre-increment operator, moves the iterator to the next word and returns it
+     * 
+     *  @return         A reference to this iterator for chaining
+     */
     word_iterator& operator++() noexcept {
         _it = std::find_if(_it, std::end(_data), isspace) + 1;
         
         return *this;
     }
 
+    /**
+     *  Post-increment operator, moves the iterator to point to the next word
+     * 
+     *  @return         A new iterator pointing to the current word
+     */
     word_iterator operator++(int) noexcept {
         auto copy = *this;
         this->operator++();
@@ -43,6 +76,11 @@ public:
         return copy;
     }
 
+    /**
+     *  Derefence operator, retrieves the current pointed word
+     * 
+     *  @return         A new view of the pointer word
+     */
     value_type operator*() const noexcept {
         auto it = std::find_if(_it, std::end(_data), isspace);
         if (it == std::end(_data)) {
